9d02a6f16be07518e3a1688b90262674a80a09fb
ssmock <ssmock@gmail.com>
2015-01-03 11:27:10 -0500

    Initial commit

---
+++ GitWithCITest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWithCITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yep");
+            Console.ReadLine();
+        }
+    }
+}
